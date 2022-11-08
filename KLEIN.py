2da0e0de5130d91fe63b91e3ef7f79e9f547a48c
XAIVER-XD <116884025+XAIVER-XD@users.noreply.github.com>
2022-11-08 20:17:13 +0600

    Add files via upload

---
+++ KLEIN.py
@@ -0,0 +1,19 @@
+#<\>!python3.11
+#-------------------Dont Edit This Script-----------------#
+import os,platform,time
+
+bitt=platform.architecture()[0]
+
+if bitt=="32bit":
+    print('[!] Your Device is 32 bit');time.sleep(1);print('\n\n[!] Your Python Version :');time.sleep(1);os.system('python --version')
+    time.sleep(2)
+    import M32.i
+
+
+elif bitt=="64bit":
+    os.system('clear');print('[!] Your Device is 64 bit');time.sleep(1);print('\n\n[!] Your Python Version :');time.sleep(1);os.system('python --version')
+    time.sleep(2)
+    import M64.i
+
+else:
+    print('\nUNKNOWN DEVICE')
